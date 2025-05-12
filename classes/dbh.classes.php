d5b1814797283fa58d1008e77f5b6faabc66d595
Fatih <64631921+fatihavci06@users.noreply.github.com>
2025-05-12 15:35:24 +0300

    test

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -5,8 +5,8 @@ class Dbh {
 	protected function connect(){
 		try {
 			$host = "localhost";
-			$user = "acaLineIzm0025";
-			$pass = "Xav3DW6HeBj!ZRr4EgdY2M";
+			$user = "root";
+			$pass = "";
 			$dbName = "lineup25academy00";
 			$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbName, $user, $pass);
 			$dbh->exec("set names utf8mb4");
