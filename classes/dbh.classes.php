7b2dd7c34a357732f8967f8cfca2b54951e7061b
epikman87 <aepikman@gmail.com>
2025-05-12 15:22:19 +0300

    canlı db

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -2,11 +2,11 @@
 
 class Dbh {
 
-	public function connect(){
+	protected function connect(){
 		try {
 			$host = "localhost";
-			$user = "root";
-			$pass = "";
+			$user = "acaLineIzm0025";
+			$pass = "Xav3DW6HeBj!ZRr4EgdY2M";
 			$dbName = "lineup25academy00";
 			$dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbName, $user, $pass);
 			$dbh->exec("set names utf8mb4");
